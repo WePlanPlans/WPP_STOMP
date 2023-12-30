c7c16768199d15948905379fca59879b8a5d4fe8
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-12-30 11:45:42 +0900

    initial commit

---
+++ src/main/java/org/tenten/tentenstomp/TenTenStompApplication.java
@@ -0,0 +1,13 @@
+package org.tenten.tentenstomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenTenStompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenTenStompApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tenten/tentenstomp/TenTenStompApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tenten.tentenstomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenTenStompApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
