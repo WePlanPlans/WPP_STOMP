35dccdc4f1827d5dad2ca156911986537b0dea44
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-02 21:59:18 +0900

    Feature : 경로 계산시 이동시간, 이동거리 추가

--- src/main/java/org/tenten/tentenstomp/global/component/OdsayComponent.java
+++ src/main/java/org/tenten/tentenstomp/global/component/OdsayComponent.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.tenten.tentenstomp.global.component.dto.response.PathInfo;
 import org.tenten.tentenstomp.global.exception.GlobalException;
 
 import java.util.List;
@@ -30,10 +31,10 @@ public class OdsayComponent {
     private String apiKey;
     private final ObjectMapper objectMapper;
     private final String BASE_URL = "https://api.odsay.com/v1/api/searchPubTransPathT";
-    public Long calculatePrice(String fromLongitude,
-                               String fromLatitude,
-                               String toLongitude,
-                               String toLatitude) {
+    public PathInfo calculatePathInfo(String fromLongitude,
+                                      String fromLatitude,
+                                      String toLongitude,
+                                      String toLatitude) {
         UriComponents uri = UriComponentsBuilder
             .fromUriString(BASE_URL)
             .queryParam("apiKey", apiKey)
@@ -55,7 +56,9 @@ public class OdsayComponent {
                 Map<String, Object> path = pathList.get(0);
                 Map<String, Object> pathInfo = (Map<String, Object>) path.get("info");
                 Integer payment = (Integer) pathInfo.get("payment");
-                return(long) payment ;
+                Double distance = (Double) pathInfo.get("totalDistance");
+                Integer totalTime = (Integer) pathInfo.get("totalTime");
+                return new PathInfo((long) totalTime, distance, (long) payment);
             } else {
                 throw new GlobalException("대중교통 경로를 조회할 수 없습니다.", CONFLICT);
             }
