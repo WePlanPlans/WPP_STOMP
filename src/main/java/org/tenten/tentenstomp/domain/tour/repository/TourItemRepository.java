bd3026b8fa7dadec2e51d332982dc2e141ebeb51
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-10 00:29:47 +0900

    Chore : 사용하지 않는 코드 제거

--- src/main/java/org/tenten/tentenstomp/domain/tour/repository/TourItemRepository.java
+++ src/main/java/org/tenten/tentenstomp/domain/tour/repository/TourItemRepository.java
@@ -1,14 +1,7 @@
 package org.tenten.tentenstomp.domain.tour.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.tenten.tentenstomp.domain.tour.entity.TourItem;
 
-import java.util.List;
-import java.util.Map;
-
 public interface TourItemRepository extends JpaRepository<TourItem, Long> {
-    @Query("SELECT ti FROM TourItem ti WHERE ti.id in :tourItemIds ORDER BY ti.id ASC")
-    public List<TourItem> findTourItemByTourItemIds(@Param("tourItems") List<Long> tourItemIds);
 }
