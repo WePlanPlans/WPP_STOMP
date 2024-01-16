8f2cc27ae17593a8eb289f272562ea0a6222368d
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-16 19:56:35 +0900

    Refactor : 여행 아이템 추가시 교통 수단 관련 값 제거

--- src/main/java/org/tenten/tentenstomp/domain/trip/dto/request/TripItemAddMsg.java
+++ src/main/java/org/tenten/tentenstomp/domain/trip/dto/request/TripItemAddMsg.java
@@ -3,12 +3,9 @@ package org.tenten.tentenstomp.domain.trip.dto.request;
 import org.tenten.tentenstomp.domain.tour.entity.TourItem;
 import org.tenten.tentenstomp.domain.trip.entity.Trip;
 import org.tenten.tentenstomp.domain.trip.entity.TripItem;
-import org.tenten.tentenstomp.global.common.enums.Transportation;
 
 import java.time.LocalDate;
 import java.util.List;
-
-import static org.tenten.tentenstomp.global.common.enums.Transportation.PUBLIC_TRANSPORTATION;
 
 public record TripItemAddMsg(
     String visitDate,
@@ -20,7 +17,6 @@ public record TripItemAddMsg(
         public static TripItem toEntity(TourItem tourItem,  Trip trip, Long seqNum, LocalDate visitDate) {
 
             return TripItem.builder()
-                .transportation(PUBLIC_TRANSPORTATION)
                 .trip(trip)
                 .price(0L)
                 .seqNum(seqNum)
