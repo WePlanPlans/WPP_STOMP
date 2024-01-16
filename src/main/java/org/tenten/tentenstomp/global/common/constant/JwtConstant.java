991c956ea559d7f0fc1df5c1ffe38a421e9122d2
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-16 17:06:45 +0900

    Refactor : Jwt에서 멤버 아이디 추출하는 로직 추가

---
+++ src/main/java/org/tenten/tentenstomp/global/common/constant/JwtConstant.java
@@ -0,0 +1,8 @@
+package org.tenten.tentenstomp.global.common.constant;
+
+public class JwtConstant {
+    public static final String AUTHORITIES_KEY = "auth";
+    public static final String BEARER_TYPE = "Bearer";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+}
