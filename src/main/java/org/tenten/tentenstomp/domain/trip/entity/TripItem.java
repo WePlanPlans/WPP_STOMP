bc82f6f943a7a5ec5892b0432be7e7ea42397b26
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-16 19:36:36 +0900

    Refactor : TripItem transportation 제거

--- src/main/java/org/tenten/tentenstomp/domain/trip/entity/TripItem.java
+++ src/main/java/org/tenten/tentenstomp/domain/trip/entity/TripItem.java
@@ -6,14 +6,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
-import org.tenten.tentenstomp.domain.member.entity.Member;
 import org.tenten.tentenstomp.domain.tour.entity.TourItem;
 import org.tenten.tentenstomp.global.common.BaseTimeEntity;
-import org.tenten.tentenstomp.global.common.enums.Transportation;
 
 import java.time.LocalDate;
 
-import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static jakarta.persistence.InheritanceType.JOINED;
 
@@ -29,8 +26,6 @@ public class TripItem extends BaseTimeEntity {
     @Column(name = "tripItemId")
     private Long id;
 
-    @Enumerated(STRING)
-    private Transportation transportation;
     private Long seqNum; // 방문 순서
     private LocalDate visitDate;
     @ColumnDefault("0")
@@ -55,12 +50,5 @@ public class TripItem extends BaseTimeEntity {
 
     public void updateVisitDate(LocalDate visitDate) {
         this.visitDate = visitDate;
-    }
-
-    public void updateTransportation(Transportation transportation) {
-        this.transportation = transportation;
-    }
-    public int compareWithSeqNum(TripItem compare) {
-        return Long.compare(this.seqNum, compare.seqNum);
     }
 }
