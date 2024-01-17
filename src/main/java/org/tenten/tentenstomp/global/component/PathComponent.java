19d96f2cb669c3eba9b7b95539ebf530defe62bc
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2024-01-17 23:09:20 +0900

    Refactor : 여정 아이템 1개 일때 경로 정보 추가

--- src/main/java/org/tenten/tentenstomp/global/component/PathComponent.java
+++ src/main/java/org/tenten/tentenstomp/global/component/PathComponent.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.tenten.tentenstomp.domain.trip.dto.response.TripPathInfoMsg;
+import org.tenten.tentenstomp.domain.trip.dto.response.TripPathInfoMsg.PathInfo;
 import org.tenten.tentenstomp.global.common.annotation.GetExecutionTime;
 import org.tenten.tentenstomp.global.common.enums.Transportation;
 import org.tenten.tentenstomp.global.component.dto.request.PathCalculateRequest;
@@ -25,29 +26,40 @@ public class PathComponent {
     public TripPathCalculationResult getTripPath(List<TripPlace> tripPlaceList, Transportation transportation) {
 
         List<PathCalculateRequest> pathCalculateRequests = toPathCalculateRequest(tripPlaceList);
-        Integer priceSum = 0;
-        List<TripPathInfoMsg> pathInfoMsgs = new CopyOnWriteArrayList<>();
-        for (PathCalculateRequest calculateRequest : pathCalculateRequests) {
-            asyncPathComponent.calculatePath(calculateRequest.from(), calculateRequest.to(), pathInfoMsgs, transportation);
-        }
-        while (true) {
-            if (pathInfoMsgs.size() == pathCalculateRequests.size()) {
-                break;
+        if (pathCalculateRequests.isEmpty()) {
+            List<TripPathInfoMsg> pathInfoMsgs = new ArrayList<>();
+            TripPlace tripPlace = tripPlaceList.get(0);
+            pathInfoMsgs.add(new TripPathInfoMsg(
+                tripPlace.tripItemId(), tripPlace.tripItemId(), tripPlace.seqNum(), tripPlace.seqNum(),
+                tripPlace.longitude(), tripPlace.latitude(), tripPlace.longitude(), tripPlace.latitude(), new PathInfo(0, 0.0, 0L)
+            ));
+            return new TripPathCalculationResult(0, pathInfoMsgs);
+        } else {
+            Integer priceSum = 0;
+            List<TripPathInfoMsg> pathInfoMsgs = new CopyOnWriteArrayList<>();
+            for (PathCalculateRequest calculateRequest : pathCalculateRequests) {
+                asyncPathComponent.calculatePath(calculateRequest.from(), calculateRequest.to(), pathInfoMsgs, transportation);
             }
-        }
-        for (TripPathInfoMsg tpm : pathInfoMsgs) {
-            if (tpm.pathInfo() != null) {
-                priceSum += tpm.pathInfo().price();
+            while (true) {
+                if (pathInfoMsgs.size() == pathCalculateRequests.size()) {
+                    break;
+                }
             }
-        }
-        pathInfoMsgs.sort((a, b) -> {
-            if (!a.fromSeqNum().equals(b.fromSeqNum())) {
+            for (TripPathInfoMsg tpm : pathInfoMsgs) {
+                if (tpm.pathInfo() != null) {
+                    priceSum += tpm.pathInfo().price();
+                }
+            }
+            pathInfoMsgs.sort((a, b) -> {
+                if (!a.fromSeqNum().equals(b.fromSeqNum())) {
 
-                return Integer.parseInt(Long.toString(a.fromSeqNum() - b.fromSeqNum()));
-            }
-            return Integer.parseInt(Long.toString(a.fromTripItemId() - b.fromTripItemId()));
-        });
-        return new TripPathCalculationResult(priceSum, pathInfoMsgs);
+                    return Integer.parseInt(Long.toString(a.fromSeqNum() - b.fromSeqNum()));
+                }
+                return Integer.parseInt(Long.toString(a.fromTripItemId() - b.fromTripItemId()));
+            });
+            return new TripPathCalculationResult(priceSum, pathInfoMsgs);
+        }
+
 
     }
 
