b0b0c6a3e2c7fef5e6f2399bb515b6bb169a34a6
유현 <yh7119@gmail.com>
2024-01-24 16:22:31 +0900

    Fix: 불필요한 커밋 제거

--- src/test/java/org/tenten/tentenstomp/TenTenStompApplicationTests.java
+++ src/test/java/org/tenten/tentenstomp/TenTenStompApplicationTests.java
@@ -2,7 +2,7 @@ package org.tenten.tentenstomp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//
 //@SpringBootTest
 //class TenTenStompApplicationTests {
 //
